99cc36e7b2a5a344b2d514e5e868f19e576b0e10
J.G <7927364+javiergarza@users.noreply.github.com>
2019-06-06 01:12:16 -0700

    Update get-im-policy-name.sh

--- helpers/get-im-policy-name.sh
+++ helpers/get-im-policy-name.sh
@@ -1,5 +1,5 @@
-POLICYNAME=`akamai property retrieve $1 2> /dev/null|grep policyTokenDefault|sed 's/.*: //;s/,//;s/"//g'`
-ASSETID=`akamai property search $1 2> /dev/null | jq '.versions.items[0].assetId'|sed 's/.*_//;s/"//'`
+POLICYNAME=`akamai property --section default retrieve $1 2> /dev/null|grep policyTokenDefault|sed 's/.*: //;s/,//;s/"//g'`
+ASSETID=`akamai property --section default search $1 2> /dev/null | jq '.versions.items[0].assetId'|sed 's/.*_//;s/"//'`
 
 #echo POLICYNAME=${POLICYNAME}
 if [ -z ${POLICYNAME} ] ; then
